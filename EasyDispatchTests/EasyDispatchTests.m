2c69bba99f7710c3cf037249bf820b745403f100
Scott <Scott@Scott-Liu.com>
2018-08-26 05:21:13 -0500

    Added a naive test-case

--- EasyDispatchTests/EasyDispatchTests.m
+++ EasyDispatchTests/EasyDispatchTests.m
@@ -6,7 +6,9 @@
 //  Copyright © 2018 Scott Liu. All rights reserved.
 //
 
+#import "EasyDispatch.h"
 #import <XCTest/XCTest.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface EasyDispatchTests : XCTestCase
 
@@ -27,6 +29,22 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    NSString *description = [NSString stringWithFormat:@"%s%d", __FUNCTION__, __LINE__];
+    XCTestExpectation *exp = [self expectationWithDescription:description];
+    printf("==========\n");
+    printf("Hello world\n");
+    printf("==========\n");
+    // CACurrentMediaTime() is a device time that doesn't change with network syncs
+    CFTimeInterval startTime = CACurrentMediaTime();
+    delay(3, ^{
+        CFTimeInterval endTime = CACurrentMediaTime();
+        printf("testing 3 secs\n");
+        printf("time elapsed %f\n", endTime - startTime);
+        [exp fulfill];
+    });
+    // Wait for the async request to complete
+    [self waitForExpectationsWithTimeout:60 handler: nil];
+    printf("end\n");
 }
 
 - (void)testPerformanceExample {
